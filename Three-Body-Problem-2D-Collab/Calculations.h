aa96da51c96c3a511fb97625a13cfc6fdc5af7a4
Catpotato42 <52795409+Catpotato42@users.noreply.github.com>
2025-10-29 23:25:58 -0700

    removed boost, made sure calculations compiles

--- Three-Body-Problem-2D-Collab/Calculations.h
+++ Three-Body-Problem-2D-Collab/Calculations.h
@@ -2,6 +2,25 @@
 #include <iostream>
 #include <vector>
 
+//both structs are small so don't strictly need their own header files, so included here for simplicity.
+//used in main file when user inputs initial values, is packaged into State struct.
+struct PlanetInfo {
+	int xPos;
+	int yPos;
+	float xVel;
+	float yVel;
+	float mass; // >0, enforce this.
+};
+
+struct State {
+	State(int numPlanets) {
+		//not necessary, would max out at 10 planets anyways so memory isn't a big issue here. However, this lets
+		//me show off that I know how to construct member variables in constructor.
+		states.resize(numPlanets);
+	}
+	std::vector<PlanetInfo> states; //vector is good cause need dynamic sizing
+};
+
 class Calculations {
 public:
 	//initial constructor of our calculations, after this our values for each planet are set by user input.
@@ -18,24 +37,4 @@ public:
 	}
 	int timeStep; //in ms
 	//Function to step our ODE taking in and outputting an array of State structs for each timestep.
-};
-
-//both structs are small so don't strictly need their own header files, so included here for simplicity.
-struct State {
-	State(int numPlanets) {
-		//not necessary, would max out at 10 planets anyways so memory isn't a big issue here. However, this lets
-		//me show off that I know how to construct member variables in constructor.
-		states.resize(numPlanets);
-	}
-	std::vector<PlanetInfo> states; //vector is good cause need dynamic sizing
-};
-
-
-//used in main file when user inputs initial values, is packaged into State struct.
-struct PlanetInfo {
-	int xPos;
-	int yPos;
-	float xVel;
-	float yVel;
-	float mass; // >0, enforce this.
 };
