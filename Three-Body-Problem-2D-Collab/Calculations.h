9cfe01562ef765e5462abf459b93322e88470a41
burnsa2024 <awburns2024@gmail.com>
2025-11-04 13:47:01 -0800

    solve function fixes
    fixed output format, various bugs, etc

--- Three-Body-Problem-2D-Collab/Calculations.h
+++ Three-Body-Problem-2D-Collab/Calculations.h
@@ -6,8 +6,8 @@
 //both structs are small so don't strictly need their own header files, so included here for simplicity.
 //used in main file when user inputs initial values, is packaged into State struct.
 struct PlanetInfo {
-	int xPos;
-	int yPos;
+	float xPos;
+	float yPos;
 	float xVel;
 	float yVel;
 	float mass; // >0, enforce this.
@@ -29,50 +29,49 @@ public:
 		: initialState(planets) //construct initial state
 	{
 		this->planets = planets;
-		this->timeStep = 1000 / fps; //fps to ms
-		this->simLength = 1000 * simLength; //seconds to ms
+		this->timeStep = 1000.0 / fps; //fps to ms
+		this->simLength = 1000.0 * simLength; //seconds to ms
 	};
 	//Function that sets initial values for each planet based on user input.
 	void setInitialValues(std::vector<PlanetInfo> initVals) {
 		initialState.states = initVals;
+		std::cout << initialState.states[0].xPos << std::endl;
 	}
 	//Function to step our ODE outputting an array of State structs for each timestep (using our state structs, time intervals, and simulation length).
-	std::vector<std::vector<std::pair<double, double>>> solve() {
-		std::vector<std::vector<std::pair<double, double>>> solution;
-		
-
+	std::vector<std::vector<std::pair<float, float>>> solve() {
+		std::vector<std::vector<std::pair<float, float>>> solution(planets);
 		for(int k = 0; k < simLength; k += timeStep) {
 			for(int i = 0; i < planets; i++) {
+				std::vector<std::pair<float, float>> planetPosition;
+				double netAccelerationX = 0;
+				double netAccelerationY = 0;
 				for (int j = 0; j < planets; j++) {
-					double netAccelerationX = 0;
-					double netAccelerationY = 0;
 					double distance = distanceCalculation(initialState.states[j], initialState.states[i]);
 					if (distance != 0) {
 						//calculate accelerations
 						netAccelerationY += G * (initialState.states[j].mass * (initialState.states[j].yPos - initialState.states[i].yPos) / std::pow(distance, 3));
 						netAccelerationX += G * (initialState.states[j].mass * (initialState.states[j].xPos - initialState.states[i].xPos) / std::pow(distance, 3));
-						
-						//update velocities
-						initialState.states[i].xVel += netAccelerationX * (timeStep / 1000);
-						initialState.states[i].yVel += netAccelerationY * (timeStep / 1000);
-
-						//update positions
-						initialState.states[i].xPos += initialState.states[i].xVel * (timeStep / 1000);
-						initialState.states[i].yPos += initialState.states[i].yVel * (timeStep / 1000);
-						solution[i].push_back({ initialState.states[i].xPos, initialState.states[i].yPos });
-
-					}
 					
+					}
 				}
+				//update velocities
+				initialState.states[i].xVel += netAccelerationX * (timeStep / 1000.0);
+				initialState.states[i].yVel += netAccelerationY * (timeStep / 1000.0);
+				std::cout << "velocity: " << initialState.states[i].xVel << ", " << initialState.states[i].yVel << std::endl;
+				//update positions
+				initialState.states[i].xPos += initialState.states[i].xVel * (timeStep / 1000.0);
+				initialState.states[i].yPos += initialState.states[i].yVel * (timeStep / 1000.0);
+				std::cout << "new position " << initialState.states[i].xPos << ", " << initialState.states[i].yPos << std::endl;
+				std::pair<float, float> insert(initialState.states[i].xPos, initialState.states[i].yPos);
+				solution[i].push_back(insert);
 			}
-			
 			
 		}
 		return solution;
 	}
 private:
 	//struct holding an array of planet infos.
-	double G = 6.67430e-11; //gravitational constant
+	double G = 6.67430; //gravitational constant
 	State initialState;
 	int timeStep; //in ms
 	int simLength; //in ms
--- Three-Body-Problem-2D-Collab/test.cpp
+++ Three-Body-Problem-2D-Collab/test.cpp
@@ -1,13 +1,20 @@
 #include "Calculations.h"
+#include <fstream>
 
 int main() {
 	Calculations test(3, 60, 10);
-	std::vector<PlanetInfo> initialValues = { {0, 0, 5, 2, 10}, {5, 5, -1, 4, 10}, {1, 10, 3, -1, 10} };
+	std::cout << "hi" << std::endl;
+	std::vector<PlanetInfo> initialValues = { {1, 0, 5, 2, 10}, {5, 5, -1, 4, 10}, {1, 10, 3, -1, 10} };
+	
+	std::ofstream fileFile;
+	fileFile.open("fileFile.txt");
+
 	test.setInitialValues(initialValues);
+
 	auto solution = test.solve();
-	for (auto c : solution) {
-		for (auto d : c) {
-			std::cout << d.first << ", " << d.second << std::endl;
-		}
+	
+	for(int i = 0; i < solution[0].size(); i++) {
+		fileFile << "(" << solution[0][i].first << ", " << solution[0][i].second << ")" << "   " << "(" << solution[1][i].first << ", " << solution[1][i].second << ")" << "   "<< "(" << solution[0][i].first << ", " << solution[0][i].second << ")" << std::endl;
 	}
+	fileFile.close();
 }
